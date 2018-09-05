050adccd92041237f3bba4e7c1f21741f76ee072
Austin-Schmidli <32604113+Austin-Schmidli@users.noreply.github.com>
2018-09-04 18:27:38 -0700

    message updated for cache test

--- repoTest/index.js
+++ repoTest/index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World! 2';
+  let message = req.query.message || req.body.message || 'This is a message.';
   res.status(200).send(message);
 };
